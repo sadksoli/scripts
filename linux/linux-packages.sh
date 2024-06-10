a2b5ad9f99389bc136b5cc712a333766d3ff41b6
Danila Vyalikov <99220305+sadksoli@users.noreply.github.com>
2024-06-10 16:29:17 +0300

    Create linux-packages.sh

---
+++ linux/linux-packages.sh
@@ -0,0 +1,15 @@
+sudo apt update
+
+sudo apt install dkms gcc -y
+
+sudo apt upgrade dpkg -y
+
+sudo apt install wget curl nano gzip python -y
+
+
+sudo apt install flatpak -y
+
+
+
+
+sudo apt update
