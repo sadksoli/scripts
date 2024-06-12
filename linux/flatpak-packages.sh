8cd06f8fbf0c6663fe680b0ec7a7b51989e8e104
Danila Vyalikov <99220305+sadksoli@users.noreply.github.com>
2024-06-12 13:35:31 +0300

    Create flatpak-packages.sh

---
+++ linux/flatpak-packages.sh
@@ -0,0 +1,37 @@
+sudo apt update
+
+
+sudo apt install flatpak -y
+
+
+#system-tools
+flatpak install flathub io.github.flattool.Warehouse
+
+flatpak install flathub com.github.tchx84.Flatseal
+
+flatpak install flathub it.mijorus.gearlever
+
+flatpak install flathub io.github.dvlv.boxbuddyrs
+
+flatpak install flathub org.gnome.Boxes
+
+flatpak install flathub io.missioncenter.MissionCenter
+
+#user-tools
+flatpak install flathub io.github.ungoogled_software.ungoogled_chromium
+
+flatpak install flathub org.videolan.VLC
+
+flatpak install flathub com.transmissionbt.Transmission
+
+flatpak install flathub com.obsproject.Studio
+
+flatpak install flathub org.libreoffice.LibreOffice
+
+flatpak install flathub org.gimp.GIMP
+
+
+#games or win applications
+flatpak install flathub com.usebottles.bottles
+
+flatpak install flathub com.github.Matoking.protontricks
