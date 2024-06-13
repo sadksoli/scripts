08821928c00fe5af8333d22099f134e0c8fa18ed
Danila Vyalikov <99220305+sadksoli@users.noreply.github.com>
2024-06-13 09:43:55 +0300

    Update linux-packages.sh

--- linux/linux-packages.sh
+++ linux/linux-packages.sh
@@ -1,9 +1,11 @@
 sudo apt update
 
 #system-tools
-sudo apt install dkms cmake gcc -y
+sudo apt update python -y
+sudo apt install dkms cmake gcc git curl g++ ninja-build -y
 
 sudo apt upgrade dpkg -y
+sudo dpkg --add-architecture i386
 
 sudo apt install wget curl nano gzip python -y
 
