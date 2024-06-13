50e60fe18e189d27c2f298e919c6a522919399ec
Danila Vyalikov <99220305+sadksoli@users.noreply.github.com>
2024-06-13 09:44:58 +0300

    Update linux-packages.sh

--- linux/linux-packages.sh
+++ linux/linux-packages.sh
@@ -1,13 +1,13 @@
 sudo apt update
 
 #system-tools
-sudo apt update python -y
+sudo apt update python3 -y
 sudo apt install dkms cmake gcc git curl g++ ninja-build -y
 
 sudo apt upgrade dpkg -y
 sudo dpkg --add-architecture i386
 
-sudo apt install wget curl nano gzip python -y
+sudo apt install wget curl nano gzip -y
 
 
 sudo apt install flatpak -y
