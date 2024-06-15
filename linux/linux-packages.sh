24b1c3d63dc6c69fd8b12d2054c7e7dec276b44d
Danila Vyalikov <99220305+sadksoli@users.noreply.github.com>
2024-06-16 02:06:19 +0300

    Update linux-packages.sh

--- linux/linux-packages.sh
+++ linux/linux-packages.sh
@@ -6,15 +6,9 @@ sudo apt update -y
 sudo apt update python3 -y
 sudo apt install dkms wget cmake build-essential libelf-dev linux-headers-`uname -r` gcc git curl g++ ninja-build -y
 
+#dpkg
 sudo apt upgrade dpkg -y
 sudo dpkg --add-architecture i386
 
-sudo apt install gzip -y
-
-
-sudo apt install flatpak -y
-
-
-
-
-sudo apt update -y
+#user-tools
+sudo apt install gzip flatpak -y
