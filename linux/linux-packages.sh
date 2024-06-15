68b2a091219b77347f49f259809f650e73d42f19
Danila Vyalikov <99220305+sadksoli@users.noreply.github.com>
2024-06-16 02:01:22 +0300

    Update linux-packages.sh

--- linux/linux-packages.sh
+++ linux/linux-packages.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 
 #system-tools
 sudo apt update python3 -y
-sudo apt install dkms wget cmake gcc git curl g++ ninja-build -y
+sudo apt install dkms wget cmake build-essential libelf-dev linux-headers-`uname -r` gcc git curl g++ ninja-build -y
 
 sudo apt upgrade dpkg -y
 sudo dpkg --add-architecture i386
