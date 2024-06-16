42c1305242521abe3d805365a115ae5c4debd703
Danila Vyalikov <99220305+sadksoli@users.noreply.github.com>
2024-06-16 15:46:11 +0300

    Create win-packages.ps1

---
+++ windows/win-packages.ps1
@@ -0,0 +1,13 @@
+#system-tools
+
+#.net-sdk
+iwr -URL https://download.visualstudio.microsoft.com/download/pr/b6f19ef3-52ca-40b1-b78b-0712d3c8bf4d/426bd0d376479d551ce4d5ac0ecf63a5/dotnet-sdk-8.0.302-win-x64.exe -Outfile "%username%\Downloads\net-sdk-8.exe"
+
+#adb tools
+iwr -URL https://dl.google.com/android/repository/platform-tools-latest-windows.zip -Outfile "%username%\Downloads\adb.zip"
+
+#java 8
+iwr -URL https://javadl.oracle.com/webapps/download/AutoDL?BundleId=249851_43d62d619be4e416215729597d70b8ac -Outfile "%username%\Downloads\java8.exe"
+
+#java 17
+iwr -URL https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.exe -Outfile "%username%\Downloads\java17.exe"
