1c8176a83382c2735862647dea8c96ba008af9b3
Danila Vyalikov <99220305+sadksoli@users.noreply.github.com>
2024-06-17 00:06:55 +0300

    Update win-packages.ps1

--- windows/win-packages.ps1
+++ windows/win-packages.ps1
@@ -4,6 +4,7 @@
 iwr -URL https://download.visualstudio.microsoft.com/download/pr/b6f19ef3-52ca-40b1-b78b-0712d3c8bf4d/426bd0d376479d551ce4d5ac0ecf63a5/dotnet-sdk-8.0.302-win-x64.exe -Outfile "%username%\Downloads\net-sdk-8.exe"
 
 #adb tools
+iwr -URL https://dl.google.com/android/repository/usb_driver_r13-windows.zip -Outfile "%username%\Downloads\google-usb-driver.zip"
 iwr -URL https://dl.google.com/android/repository/platform-tools-latest-windows.zip -Outfile "%username%\Downloads\adb.zip"
 
 #java 8
