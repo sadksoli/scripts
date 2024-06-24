6c4ad987df933b6ba1dda879407c73110f4f39ac
Danila Vyalikov <99220305+sadksoli@users.noreply.github.com>
2024-06-24 19:48:03 +0300

    Update linux-packages.sh

--- linux/linux-packages.sh
+++ linux/linux-packages.sh
@@ -7,11 +7,14 @@ sudo apt upgrade -y
 sudo apt upgrade dpkg -y
 sudo dpkg --add-architecture i386
 
+
 #system-tools
 sudo apt install dkms wget curl -y
+
 
 #building-components
 sudo apt install cmake build-essential libelf-dev linux-headers-`uname -r` gcc clang git meson g++ mono-devel ninja-build -y
 
+
 #user-tools
-sudo apt install gzip flatpak -y
+sudo apt install gzip wine winetricks -y
