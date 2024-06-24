f270456983944a03adf1bfe4fa89af9af0538d7d
Danila Vyalikov <99220305+sadksoli@users.noreply.github.com>
2024-06-24 18:27:23 +0300

    Update linux-packages.sh

--- linux/linux-packages.sh
+++ linux/linux-packages.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-sudo apt update -y
-
-#system-tools
-sudo apt update python3 -y
-sudo apt install dkms wget curl cmake build-essential libelf-dev linux-headers-`uname -r` gcc clang git meson g++ ninja-build -y
+sudo apt-get update -y
+sudo apt upgrade -y
 
 #dpkg
 sudo apt upgrade dpkg -y
 sudo dpkg --add-architecture i386
+
+#system-tools
+sudo apt install dkms wget curl -y
+
+#building-components
+sudo apt install cmake build-essential libelf-dev linux-headers-`uname -r` gcc clang git meson g++ ninja-build -y
 
 #user-tools
 sudo apt install gzip flatpak -y
