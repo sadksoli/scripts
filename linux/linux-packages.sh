d60228eea5769530ff4dece5f2d11cc9e12052b0
Danila Vyalikov <99220305+sadksoli@users.noreply.github.com>
2024-06-30 16:22:25 +0300

    Update linux-packages.sh

--- linux/linux-packages.sh
+++ linux/linux-packages.sh
@@ -8,11 +8,11 @@ sudo dpkg --add-architecture i386
 
 
 #system-tools
-sudo apt install repo git dkms wget curl -y
+sudo apt install repo git dkms wget curl zlib -y
 
 
 #building-components
-sudo apt install cmake build-essential libelf-dev linux-headers-`uname -r` gcc g++ clang meson mono-devel ninja-build -y
+sudo apt-get install cmake build-essential libelf-dev linux-headers-`uname -r` gcc g++ clang meson mono-devel ninja-build -y
 
 
 #user-tools
